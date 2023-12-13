036e4ae0979856159e0fb643083e89e9d9f7d412
Royce Kraakman <49066208+roycekraakman@users.noreply.github.com>
2023-12-13 17:22:42 +0100

    Some starting plots for testing, nothing special

--- analysis/main.R
+++ analysis/main.R
@@ -9,4 +9,16 @@ df <- con %>%
   dbReadTable("songs") %>%
   as_tibble()
 
-df
+theme_set(theme_minimal())
+
+df %>%
+  ggplot(aes(x = tempo)) +
+  geom_histogram()
+
+df %>%
+  ggplot(aes(x = scale)) +
+  geom_bar()
+
+df %>%
+  ggplot(aes(x = key)) +
+  geom_bar()
